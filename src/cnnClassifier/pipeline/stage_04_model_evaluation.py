5c8f5fa3efc6437f28e255ff05a3a615e3b3a271
entbappy <entbappy73@gmail.com>
2023-10-03 10:35:34 +0600

    dvc added

--- src/cnnClassifier/pipeline/stage_04_model_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation.py
@@ -17,7 +17,7 @@ class EvaluationPipeline:
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-        evaluation.log_into_mlflow()
+        # evaluation.log_into_mlflow()
 
 
 
